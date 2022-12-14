8031b1446d9872b605257f4180bd08f18cdd0b6a
Lara Dias <larandias1d@gmail.com>
2022-12-13 21:47:59 -0300

    criando teste - testando valor 1

---
+++ src/tests.ts
@@ -0,0 +1,16 @@
+import { Romanos } from './/romanos';
+
+const sutConversor = new Romanos();
+
+describe("Roman to Int Conversor test suite", () =>{
+
+    beforeEach(() => {
+        jest.resetAllMocks();
+    });
+
+    it("Should return 1 when the input is I", () =>{
+        expect(sutConversor.romanToInt("I")).toBe(1);
+    });
+
+   
+})
